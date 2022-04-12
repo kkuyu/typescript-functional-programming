7fe89fe1693eccf3cc44b8ab11e7fb48c9ab90b3
kkuyu <devils67@naver.com>
2022-04-12 13:38:57 +0900

    part3 - 할인가 적용

---
+++ part3/src/clip4-1.ts
@@ -0,0 +1,73 @@
+import "./index.css";
+import { cart, Item } from "./cart";
+
+const stockItem = (item: Item): string => {
+  let saleText = "";
+  let discountPrice = 0;
+  if (item.discountPrice !== undefined) {
+    discountPrice = item.discountPrice;
+    saleText = `(${item.discountPrice}원 할인)`;
+  }
+  return `
+    <li>
+      <h2>${item.name}</h2>
+      <div>가격: ${item.price - discountPrice}원 ${saleText}</div>
+      <div>수량: ${item.quantity}상자</div>
+    </li>
+  `;
+};
+
+const outOfStockItem = (item: Item): string => `
+  <li class='gray'>
+    <h2>${item.name} (품절)</h2>
+    <div class='strike'>가격: ${item.price}원</div>
+    <div class='strike'>수량: ${item.quantity}상자</div>
+  </li>
+`;
+
+const makeItem = (item: Item): string => {
+  if (item.outOfStock) return outOfStockItem(item);
+  return stockItem(item);
+};
+
+const totalCalculator = (list: Array<Item>, getValue: (item: Item) => number) => {
+  return list
+    .filter((item) => item.outOfStock === false)
+    .map(getValue)
+    .reduce((total, value) => total + value, 0);
+};
+
+const totalCount = (list: Array<Item>): string => {
+  const totalCount = totalCalculator(list, (item) => item.quantity);
+  return `<h2>전체 수량: ${totalCount}상자`;
+};
+
+const totalPrice = (list: Array<Item>): string => {
+  const totalPrice = totalCalculator(list, (item) => item.price * item.quantity);
+  const totalDiscountPrice = totalCalculator(list, (item) => {
+    let discountPrice = 0;
+    if (item.discountPrice !== undefined) {
+      discountPrice = item.discountPrice;
+    }
+    return discountPrice * item.quantity;
+  });
+  return `<h2>전체 가격: ${totalPrice - totalDiscountPrice}원 (총 ${totalDiscountPrice}원 할인)`;
+};
+
+const list = (list: Array<Item>) => {
+  return `
+    <ul>
+    ${list.map(makeItem).reduce((tags, tag) => tags + tag, "")}
+    </ul>
+  `;
+};
+
+const app = document.getElementById("app");
+if (app != null) {
+  app.innerHTML = `
+  <h1>장바구니</h1>
+  ${list(cart)}
+  ${totalCount(cart)}
+  ${totalPrice(cart)}
+  `;
+}
