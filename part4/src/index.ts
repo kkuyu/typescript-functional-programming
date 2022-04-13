3ddbb3279e891c18ca4c63fbda777b0215d20fc4
kkuyu <devils67@naver.com>
2022-04-13 13:33:10 +0900

    part4 - initialize

---
+++ part4/src/index.ts
@@ -0,0 +1,10 @@
+import "./styles.css";
+import * as C from "./clip3-0";
+
+C.main();
+
+const app = document.getElementById("app");
+if (app !== null) {
+  app.innerHTML = `
+  <h1>Hello!</h1>`;
+}
