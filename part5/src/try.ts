d4a70d2372cb0df30f1fe76c4eb4a849662092d1
kkuyu <devils67@naver.com>
2022-04-14 13:52:42 +0900

    part5 - try 타입

---
+++ part5/src/try.ts
@@ -0,0 +1,38 @@
+type Success<R> = {
+  readonly _tag: "success";
+  readonly result: R;
+};
+
+type Failed<E> = {
+  readonly _tag: "failed";
+  readonly error: E;
+};
+
+export type Try<E, R> = Failed<E> | Success<R>;
+
+// never: 안쓰는 타입 파라미터가 리턴 타입에서 사용될 때
+// unknown: 안쓰는 타입 파라미터가 인자의 타입에서 사용될 때
+export const success = <R>(result: R): Try<never, R> => ({
+  _tag: "success",
+  result,
+});
+
+export const failed = <E>(error: E): Try<E, never> => ({
+  _tag: "failed",
+  error,
+});
+
+export const isSuccess = <R>(ta: Try<unknown, R>): ta is Success<R> => {
+  return ta._tag === "success";
+};
+
+export const isFailed = <E>(ta: Try<E, unknown>): ta is Failed<E> => {
+  return ta._tag === "failed";
+};
+
+export const getOrElse = <E, R>(ta: Try<E, R>, defaultValue: (e: E) => R): R => {
+  // 에러가 있을 경우 에러에 기반하여 기본 값을 결정한다.
+  if (isFailed(ta)) return defaultValue(ta.error);
+  // 결과가 성공이라면 해당 값을 사용한다.
+  return ta.result;
+};
